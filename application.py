5c4fa02dc7675b2b38781f0cc98508f21060aa49
sumeet agrawal <sumeet.agrawal@microsoft.com>
2020-06-21 15:17:52 +0530

    Commeneted name == main

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def hello():
     return "Hello World!. This is a home page"
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
 
-    app.run(debug=True)
+#    app.run(debug=True)
 
