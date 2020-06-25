070253057aaedb898fa21a907bcbbe562bcb72d2
Sumeet Agrawal <sumeetagrawal.srm@gmail.com>
2020-06-25 12:25:47 +0530

    Update run_awesome_streamlit_with_ping.sh

--- scripts/run_awesome_streamlit_with_ping.sh
+++ scripts/run_awesome_streamlit_with_ping.sh
@@ -3,4 +3,5 @@
 # See https://lnx.azurewebsites.net/python-app-on-azure-web-apps-frequently-restarts/
 # python "scripts/ping_awesome_streamlit.py" &
 # top -d 60 -b &
+RUN echo "Invoked Streamlit shell" 
 streamlit run app.py
