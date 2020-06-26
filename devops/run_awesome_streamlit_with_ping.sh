82d5aa706c03f0424480d67f22fcd8951de58460
Sumeet Agrawal <sumeetagrawal.srm@gmail.com>
2020-06-26 15:37:59 +0530

    Update run_awesome_streamlit_with_ping.sh

--- devops/run_awesome_streamlit_with_ping.sh
+++ devops/run_awesome_streamlit_with_ping.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # The ping job will help keep awesome-streamlit.org alive.
 # See https://lnx.azurewebsites.net/python-app-on-azure-web-apps-frequently-restarts/
-# python "scripts/ping_awesome_streamlit.py" &
-# top -d 60 -b &
+python "ping_test.py" &
+top -d 60 -b &
 echo "Invoked Streamlit shell" 
 streamlit run app.py
